00f7cda661b5721f0cdbfcfb8c640cc349ed5bbe
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-17 15:19:41 -0400

    test: add test for encrypt message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(3, 'thomas')
+
+    assert encrypt_message("thomas", 3) == "sam_oht"
+
+    assert encrypt_message("12345", 0) == "54321"
