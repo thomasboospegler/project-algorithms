263f6ac781213263c25fd84bcf34aa3504c54649
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-17 15:13:28 -0400

    feat: add study_shedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        return len(
+            [
+                period
+                for period in permanence_period
+                if period[0] <= target_time <= period[1]
+            ]
+        )
+    except TypeError:
+        return None
