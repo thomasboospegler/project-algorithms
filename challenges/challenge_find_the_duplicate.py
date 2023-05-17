ce364cfedef53c8c2649cc2815f00b0d932a60e9
Thomas Boos Pegler <tboospegler@gmail.com>
2023-05-17 16:14:15 -0400

    feat: add find_duplicate function

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+    for i in range(len(nums)):
+        if (
+            i + 1 >= len(nums)
+            or not isinstance(nums[i + 1], int)
+            or not isinstance(nums[i], int)
+            or nums[i] < 0
+        ):
+            return False
+        if nums[i] == nums[i + 1]:
+            return nums[i]
+    return False
